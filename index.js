5e1dde65f95223d846d9251ae2bd013d2fa14dcf
Tim Scott <0timothy.scott0@gmail.com>
2021-03-10 19:52:12 -0500

    establish db connection

---
+++ index.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS
+});
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting to db: ' + err.stack);
+		return;
+	}
+
+	console.log('connected to db as id ' + connection.threadId);
+
+	connection.end();
+});
