ff0335fb3626b2c474881d30a250286a51202669
Tim Scott <0timothy.scott0@gmail.com>
2021-03-10 21:37:00 -0500

    connect in index.js instead of db.js

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+const { connection, tables } = require("./lib/db");
 const mainMenu = require("./lib/menus/main");
+const displayData = require("./lib/displayData");
 
-mainMenu.getAction().then((action, table) => {
-	switch (action) {
-		// Add
-		case mainMenu.actions[0]:
-			break;
-		// View-All
-		case mainMenu.actions[1]:
-			break;
-		// Update
-		case mainMenu.actions[2]:
-			break;
-		// Delete
-		case mainMenu.actions[3]:
-			break;
+// connection
+connection.connect(async (err) => {
+	if (err) {
+		console.error('error connecting to db: ' + err.stack);
+		return;
 	}
-})
+
+	console.log('connected to db as id ' + connection.threadId);
+
+	try {
+		// cli loop
+		const { action, table } = await mainMenu.getAction();
+
+		switch (action) {
+			// Add
+			case mainMenu.actions[0]:
+				break;
+			// View-All
+			case mainMenu.actions[1]:
+				break;
+			// Update
+			case mainMenu.actions[2]:
+				break;
+			// Delete
+			case mainMenu.actions[3]:
+				break;
+		}
+	}
+	catch (err) { console.error(err) }
+
+	connection.end();
+});
