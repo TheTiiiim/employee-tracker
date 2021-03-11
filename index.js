cd2b790a3a1d93797c93673b017d10b40d0498c0
Tim Scott <0timothy.scott0@gmail.com>
2021-03-10 20:41:49 -0500

    main menu

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const connection = require("./lib/connection");
+const mainMenu = require("./lib/menus/main");
 
-connection.end();
+mainMenu.getAction().then((action, table) => {
+	switch (action) {
+		// Add
+		case mainMenu.actions[0]:
+			break;
+		// View-All
+		case mainMenu.actions[1]:
+			break;
+		// Update
+		case mainMenu.actions[2]:
+			break;
+		// Delete
+		case mainMenu.actions[3]:
+			break;
+	}
+})
