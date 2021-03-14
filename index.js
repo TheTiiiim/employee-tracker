abe017a454dfa91d5c693b8fd415ad0ce6c0990c
Tim Scott <0timothy.scott0@gmail.com>
2021-03-14 18:47:12 -0400

    implement filtering viewed items by linked columns

--- index.js
+++ index.js
@@ -5,40 +5,18 @@ async function main() {
 	try {
 		await db.init();
 
-		let exit = false;
-
+		let willContinue = false;
 		// cli loop
 		do {
-			const { action, table } = await menu.getAction();
-
-			switch (action) {
-				// View-All
-				case menu.actions[0]:
-					// TODO: create menu options to view-all or veiw grouped by linked columns
-					await db.getTable(table).displayItems();
-					break;
-				// Add
-				case menu.actions[1]:
-					await menu.createItem(table);
-					break;
-				// Update
-				case menu.actions[2]:
-					await menu.updateItem(table);
-					break;
-				// Delete
-				case menu.actions[3]:
-					await menu.deleteItem(table);
-					break;
-				case "exit":
-					exit = true;
-					break;
-			}
-		} while (!exit);
+			const command = await menu.getCommand();
+			// returns false if action === 'exit' or no command is executed
+			willContinue = await menu.doCommand(command);
+		} while (willContinue);
 	}
 	catch (err) {
 		console.error(err);
 	}
-	db.close();
+	db.close().catch(console.error);
 }
 
 main();
